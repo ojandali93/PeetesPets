9f7d2346d951f6fe58a90bb5acccc10c0e5d9179
omar jandali <omarjandali93@gmail.com>
2023-07-06 19:12:18 -0700

    Users can search pets by name and breed

--- routes/pets.js
+++ routes/pets.js
@@ -55,4 +55,12 @@ module.exports = (app) => {
       return res.redirect('/')
     });
   });
+
+  app.get('/search', (req, res) => {
+    term = new RegExp(req.query.term, 'i')
+  >
+    Pet.find({'name': term}).exec((err, pets) => {
+      res.render('pets-index', { pets: pets });
+    })
+  });
 }
